67cae1c622ea653f059b8b558c6142b9170ade04
Nova Thomas <novathomas2002@gmail.com>
2023-10-10 21:12:50 -0400

    Initial commit

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float speed = 40;
+    public GameObject bullet;
+    public Transform barrel;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    
+    public void Fire()
+    {
+        GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = speed * -barrel.forward;
+        audioSource.PlayOneShot(audioClip);
+        Destroy(spawnedBullet, 2);
+
+        StartCoroutine(waiter());
+    }
+
+    // Doesn't work
+    public void Drop()
+    {
+        StartCoroutine(waiter2());
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(0.55f);
+
+        //  Add Explosion animation
+
+        Destroy(gameObject);
+    }
+    IEnumerator waiter2()
+    {
+        yield return new WaitForSeconds(5);
+
+        //  Add Explosion animation
+
+        Destroy(gameObject);
+    }
+}
